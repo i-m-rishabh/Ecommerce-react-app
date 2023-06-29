62d192eb6f0ce6bf13aaa89271067cd79899fae8
i-m-rishabh <uniquepandey125@gmail.com>
2023-06-29 23:46:58 +0530

    added retying mechanism

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Content from "../components/Content";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -7,11 +7,31 @@ import { Button, Container, ListGroup, Spinner } from "react-bootstrap";
 const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [retrying, setRetrying] = useState(false);
+    const [retry, setRety] = useState(0);
+    useEffect(()=>{
+        let  timer;
+        setRety(0);
+        if(retrying){
+         timer = setInterval(() => {
+                setRety((prev)=>{return prev+1});
+                handleFetchMovie();
+            }, 3000);
+        }
+        return ()=>{clearInterval(timer)}
+    },[retrying])
 
     async function handleFetchMovie(){
         setIsLoading(true);
         try{
-        const response = await fetch("https://swapi.dev/api/films");
+        const response = await fetch("https://swapi.dev/api/film");
+        if(!response.ok){
+            setRetrying(true);
+            throw new Error("Something went wrong.");
+        }else{
+            setRetrying(false);
+        }
         const data = await response.json();
  
         const fetchedMovies = data.results.map((movie)=>{
@@ -21,12 +41,11 @@ const Movies = () => {
                     desc:movie.opening_crawl,
                 }
             })
-            // console.log(movies);
             setMovies(fetchedMovies);
-            setIsLoading(false);
-        }catch(err){
-            console.log(err);
+        }catch(error){
+            setError(error.message);
         }
+        setIsLoading(false);
     }
 
     return <div style={{minHeight:"100vh"}}>
@@ -34,16 +53,18 @@ const Movies = () => {
         <Content />
         <Container className="my-3 d-flex flex-column justify-content-center align-items-center" style={{minHeight:"50vh"}}>
             <Button onClick={handleFetchMovie}>Fetch Movies</Button>
-            {isLoading && <Spinner animation="border" role="status" variant="primary" size="lg" className="m-3"/>}
+            {error && <p>{error}</p>}
+            {retrying && <><p>{`retrying(${retry})...`}</p> <Button onClick={()=>{setRetrying(false)}}>cancel</Button> </>}
+            {isLoading && !error && <Spinner animation="border" role="status" variant="primary" size="lg" className="m-3"/>}
             <ListGroup className="my-2">
                 {
                     movies.map((movie)=>{
                         return <div key={movie.id}>
-                        <ListGroup.Item className="">
-                        <h3 className="text-center">{movie.title}</h3>
-                        <p className="">{movie.desc}</p>
-                </ListGroup.Item>
-                        </div>
+                                    <ListGroup.Item className="">
+                                    <h3 className="text-center">{movie.title}</h3>
+                                    <p className="">{movie.desc}</p>
+                                    </ListGroup.Item>
+                                </div>
                     })
                 }
             </ListGroup>
